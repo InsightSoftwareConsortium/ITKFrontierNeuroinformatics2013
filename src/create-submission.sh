d4245f6434ec5b3287d99880cfc5ffcba72ecad5
Matt McCormick <matt.mccormick@kitware.com>
2013-11-11 02:10:17 -0500

    Convert submitted figures to .tiff, use lzw compression.

--- src/create-submission.sh
+++ src/create-submission.sh
@@ -6,7 +6,7 @@ dexy
 
 out=output/submission
 doc=output/doc
-width=2000
+width=3000
 
 mkdir -p $out
 rm $out/*
@@ -14,14 +14,15 @@ rm $out/*
 cp output/doc/frontiers.pdf $out/ITKEnablingReproducibleResearchAndOpenScience.pdf
 
 # EPS or covert to tiff?
-cpconvert="cp"
+cpconvert="convert -compress lzw -resize ${width}x"
+epstiff=tiff
 
 convert -resize ${width}x $doc/itk_module_dependency.png $out/figure_1_itk_module_dependency.tiff
 convert -resize ${width}x $doc/itk_code_contribution.png $out/figure_2_itk_code_contribution.tiff
-$cpconvert $doc/itk_git_contributors.eps $out/figure_3_itk_git_contributors.eps
-$cpconvert $doc/gerrit_patch_set_histogram.eps $out/figure_4_gerrit_patch_set_histogram.eps
-$cpconvert $doc/gerrit_fix_ups.eps $out/figure_5_gerrit_fix_ups.eps
+$cpconvert $doc/itk_git_contributors.eps $out/figure_3_itk_git_contributors.$epstiff
+$cpconvert $doc/gerrit_patch_set_histogram.eps $out/figure_4_gerrit_patch_set_histogram.$epstiff
+$cpconvert $doc/gerrit_fix_ups.eps $out/figure_5_gerrit_fix_ups.$epstiff
 convert -resize ${width}x $doc/GerritGraphRender.png $out/figure_6_gerrit_graph_render.tiff
-$cpconvert $doc/gerrit_closeness.eps $out/figure_7_gerrit_closeness.eps
-$cpconvert $doc/insight_journal_submissions.eps $out/figure_8_insight_journal_submissions.eps
+$cpconvert $doc/gerrit_closeness.eps $out/figure_7_gerrit_closeness.$epstiff
+$cpconvert $doc/insight_journal_submissions.eps $out/figure_8_insight_journal_submissions.$epstiff
 
