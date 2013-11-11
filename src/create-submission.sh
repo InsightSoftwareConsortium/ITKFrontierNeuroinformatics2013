9cb1a1735d4db7c4bda6f0e7617e835d1632b1f3
Matt McCormick <matt.mccormick@kitware.com>
2013-11-11 03:28:12 -0500

    Format figures that render in submission.
    The will only take jpeg, tif, or tiff.  BUT, the tiff cannot have an alpha
    channel because they are placed back again in the PDF.

--- src/create-submission.sh
+++ src/create-submission.sh
@@ -15,14 +15,15 @@ cp output/doc/frontiers.pdf $out/ITKEnablingReproducibleResearchAndOpenScience.p
 
 # EPS or covert to tiff?
 cpconvert="convert -compress lzw -resize ${width}x"
+cpconvert="cp"
 epstiff=tiff
 
 convert -resize ${width}x $doc/itk_module_dependency.png $out/figure_1_itk_module_dependency.tiff
 convert -resize ${width}x $doc/itk_code_contribution.png $out/figure_2_itk_code_contribution.tiff
-$cpconvert $doc/itk_git_contributors.eps $out/figure_3_itk_git_contributors.$epstiff
-$cpconvert $doc/gerrit_patch_set_histogram.eps $out/figure_4_gerrit_patch_set_histogram.$epstiff
-$cpconvert $doc/gerrit_fix_ups.eps $out/figure_5_gerrit_fix_ups.$epstiff
+$cpconvert $doc/itk_git_contributors.tiff $out/figure_3_itk_git_contributors.$epstiff
+$cpconvert $doc/gerrit_patch_set_histogram.tiff $out/figure_4_gerrit_patch_set_histogram.$epstiff
+$cpconvert $doc/gerrit_fix_ups.tiff $out/figure_5_gerrit_fix_ups.$epstiff
 convert -resize ${width}x $doc/GerritGraphRender.png $out/figure_6_gerrit_graph_render.tiff
-$cpconvert $doc/gerrit_closeness.eps $out/figure_7_gerrit_closeness.$epstiff
-$cpconvert $doc/insight_journal_submissions.eps $out/figure_8_insight_journal_submissions.$epstiff
+$cpconvert $doc/gerrit_closeness.tiff $out/figure_7_gerrit_closeness.$epstiff
+$cpconvert $doc/insight_journal_submissions.tiff $out/figure_8_insight_journal_submissions.$epstiff
 
