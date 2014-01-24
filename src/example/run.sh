d1b86966f2c33776fc4b93967b19d39271efed1c
Matt McCormick <matt.mccormick@kitware.com>
2014-01-24 15:00:08 -0500

    Change data to MNI Colin 1998 dataset.

--- src/example/run.sh
+++ src/example/run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-fixed_image=../../data/Sim_074_T2.mha
-moving_image=../../data/Sim_094_T2.mha
-chmod +x ./build/Registration
-./build/Registration $fixed_image $moving_image
+cd ../../data
+unzip mni_colin27_1998_minc2.zip
+cd -
+
+#fixed_image=../../data/Sim_074_T2.mha
+#moving_image=../../data/Sim_094_T2.mha
+#chmod +x ./build/Registration
+#./build/Registration $fixed_image $moving_image
--- src/fetch-images.py
+++ src/fetch-images.py
@@ -5,19 +5,15 @@
 import urllib2
 import os
 
-base_url = 'http://midas3.kitware.com/midas/download/bitstream/'
-files = ['101860/Sim_094_T2.mha',
-         '101652/Sim_074_T2.mha']
+url = 'http://packages.bic.mni.mcgill.ca/mni-models/colin27/mni_colin27_1998_minc2.zip'
 
 print('fetching data')
 output_dir = os.path.join('..', 'data')
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-
-for ff in files:
-    response = urllib2.urlopen(base_url + ff)
-    data = response.read()
-    filename = os.path.join(output_dir, ff.split('/')[-1])
-    print('writing ' + filename)
-    with open(filename, 'wb') as fp:
-        fp.write(data)
+response = urllib2.urlopen(url)
+data = response.read()
+filename = os.path.join(output_dir, url.split('/')[-1])
+print('writing ' + filename)
+with open(filename, 'wb') as fp:
+    fp.write(data)
